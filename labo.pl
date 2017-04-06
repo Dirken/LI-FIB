8ed08a7bc61ac254b25ef0bcb4ae0f157e3818a1
Dirken <ricard.meyerhofer@est.fib.upc.edu>
2017-04-06 14:59:50 +0200

    cleaned code of second exercise

--- labo.pl
+++ labo.pl
@@ -54,24 +54,16 @@ eliminarRepetits([Ini|Rest], [Ini|Res]):- eliminarRepetits(Rest, Res).
 %%         ...
 %% 	write(Sol), nl.
 
-incrementa(N, N1) :-
-  N1 is N + 1.
-
-decrementa(N, N1) :-
-  N1 is N - 1.
-
-lado(N, N1) :-
-  decrementa(N, N1).
-
-lado(N, N1) :-
-  incrementa(N, N1).
+lado(N, N1) :-  N1 is N - 1.
+lado(N, N1) :-  N1 is N + 1.
 
 casas:-	  Sol = [ [1,A1,B1,C1,D1,E1],
-    [2,A2,B2,C2,D2,E2],
-    [3,A3,B3,C3,D3,E3],
-    [4,A4,B4,C4,D4,E4],
-    [5,A5,B5,C5,D5,E5]
-  ],
+                  [2,A2,B2,C2,D2,E2],
+                  [3,A3,B3,C3,D3,E3],
+                  [4,A4,B4,C4,D4,E4],
+                  [5,A5,B5,C5,D5,E5]
+                ],
+
 % member( [ n, c, p, a, b, p ]  , Sol),
 member([ _, "roja", _, _, _, "Perú" ],Sol),
 member([ _, _, _, "perro", _, "Francia" ],Sol),
@@ -80,7 +72,7 @@ member([ _, _, _, _, "ron", "China" ],Sol),
 member([ 1, _, _, _, _, "Hungría" ],Sol),
 member([ _, "verde", _, _, "coñac", _ ],Sol),
 member([ N1, "verde", _, _, _, _ ],Sol),
-incrementa(N1, N11),
+N11 is N1 + 1,
 member([ N11, "blanca", _, _, _, _ ],Sol),
 member([ _, _, "escultor", "caracol", _, _ ],Sol),
 member([ _, "amarilla", "actor", _, _, _ ],Sol),
